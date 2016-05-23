953589d74edb5f94e0f121c17eb3ce66739b959a
Ciaron Linstead <ciaron.linstead@gmail.com>
2016-05-23 16:40:00 +0200

    initial commit

---
+++ src/ofApp.cpp
@@ -0,0 +1,112 @@
+#include "ofApp.h"
+
+//----- my init function for selecting transforms  ---//
+void ofApp::init() {
+  count = 0;
+
+  // initialise array of hit counts;
+  points.resize(wh);
+  std::fill(points.begin(), points.end(), 0);
+
+  ix = ofRandom(1.0);
+  iy = ofRandom(1.0);
+}
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+    ofBackground(0);
+
+    width = ofGetWidth();
+    height = ofGetHeight();
+    wh = width * height;
+
+    init();
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+  // replace the P5 calc() function;
+  points[ofRandom(1.0) * wh] = 1;
+
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+
+  screen.grabScreen(0, 0, width, height);
+  screen.setImageType(OF_IMAGE_COLOR_ALPHA);
+
+  unsigned char * screenPixels = screen.getPixels();
+
+  for (int n=0; n<width*height; n++){
+
+      if (points[n] > 0) {
+
+          screenPixels[n*4] = 255;
+          screenPixels[(n*4)+1] = 255;
+          screenPixels[(n*4)+2] = 255;
+          screenPixels[(n*4)+3] = (int)(ofRandom(1.0)* 255);
+
+      }
+  }
+
+  screen.update();
+  screen.draw(0,0,width,height);
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+    // reset the gubbins
+    cout << "resetting" << endl;
+    init();
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
---
+++ src/ofApp.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include "ofMain.h"
+
+//#include <complex.h>
+//complex<double> a, b, c, d;
+//vector<complex<double>> tr;
+//vector<complex<double>[]> transforms;
+
+class ofApp : public ofBaseApp{
+
+    int wh; // resolution, i.e. width*height;
+    double x, y, ix, iy;
+    double min = -1.0;
+    double max = 1.0;
+
+    int count;
+    int nt = 3; // num transforms
+    int np = 5000; // num points
+
+    int width, height;
+
+    vector<ofVec2f> transforms;
+    vector<int> points;  // 'hit count' for pixels
+
+    ofImage screen;
+    //ofPixels screenPixels;
+
+	public:
+        void init();
+
+		void setup();
+		void update();
+		void draw();
+
+		void keyPressed(int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y );
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void mouseEntered(int x, int y);
+		void mouseExited(int x, int y);
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+		void gotMessage(ofMessage msg);
+		
+};
